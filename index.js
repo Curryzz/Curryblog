6522030e08d5cfafe126bcf0b5893ec10dd6efc4
curry <yangyang_name@sina.cn>
2019-11-24 20:38:14 +0800

    static file

---
+++ index.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = new express();
+app.use(express.static('./page/'));
+
+app.listen(12306,function () {
+    console.log('服务已启动')
+})
+
