c3218bacd76baa63d8d03b39f44d25dea07e34d5
Curryzz <yangyang_name@sina.cn>
2019-11-25 14:52:56 +0800

    添加编辑页面

---
+++ page/js/index.js
@@ -0,0 +1,14 @@
+var everyDay = new Vue({
+    el:'#every_day',
+    data:{
+        content:"asjhcjacjashkjchaskjhckjashckjas"
+    },
+    computed:{
+        getContent:function () {
+            return this.content;
+        }
+    },
+    created:function () {
+
+    }
+})
